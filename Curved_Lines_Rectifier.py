8d860bd9fe72875cb6ff5b4aa77da1dcaa9e5dc3
Elliot Lockerman <elliot@lockerman.info>
2014-08-03 12:06:21 -0400

    Switch to fixed px per in to counter excessive scanner cropping.

--- Curved_Lines_Rectifier.py
+++ Curved_Lines_Rectifier.py
@@ -31,10 +31,9 @@ if __name__ == "__main__" :
     original_image = scipy.ndimage.imread(file)    
 
     # Set up measurments
-    paper_width_inches = 8.5
-    circle_radius_inches = 7.213
     original_width_pixels = original_image.shape[1]
-    pixels_per_inch = original_width_pixels / paper_width_inches
+    circle_radius_inches = 7.213
+    pixels_per_inch = 300
     circle_radius_pixels = 7.213 * pixels_per_inch
     
     
