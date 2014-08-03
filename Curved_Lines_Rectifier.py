e4d946845d209d198600951b08d19a1b5ada7978
Elliot Lockerman <elliot@lockerman.info>
2014-08-02 22:01:52 -0400

    Changed output name to input with _output.png catted.

--- Curved_Lines_Rectifier.py
+++ Curved_Lines_Rectifier.py
@@ -11,7 +11,7 @@ import math
 
 
 def mapping(output_coords):
-
+    
 
     input_coords = (output_coords[0] + (-1 * (math.sqrt(2164 ** 2 - (output_coords[1] - 1275) ** 2) - 2164)),output_coords[1],output_coords[2])
         
@@ -22,11 +22,15 @@ def mapping(output_coords):
 if __name__ == "__main__" :
     file = argv[1]
     original = scipy.ndimage.imread(file)
-          
+    
+    test = "test_variable"      
     
     transformed = scipy.ndimage.interpolation.geometric_transform(original, mapping)
             
-    scipy.misc.imsave('output.png',transformed)
+            
+    dot_index = file.rfind('.')
+    output_name = file[0:dot_index - 1] + '_output.png'
+    scipy.misc.imsave(output_name,transformed)
     
     transformed_render = plt.imshow(transformed)
     plt.gray()
