7979f0b0abaed6772e82c6f8d818c6b1c78e951c
Elliot Lockerman <elliot@lockerman.info>
2014-08-03 12:13:36 -0400

    Refactoring.

--- Curved_Lines_Rectifier.py
+++ Curved_Lines_Rectifier.py
@@ -10,45 +10,59 @@ import math
 
 
 
-def mapping(output_coords):
+
+
+class Curved_Lines_Rectifier:
     
-    input_coords = (output_coords[0] + (-1 * (math.sqrt(circle_radius_pixels ** 2 - (output_coords[1] - (original_width_pixels / 2)) ** 2) - circle_radius_pixels)), output_coords[1])
+    def __init__(self):
+
+        # Load up the image
+        file = argv[1]
+        original_image = scipy.ndimage.imread(file)    
+        
+        # Set up measurments
+        self.original_width_pixels = original_image.shape[1]
+        self.circle_radius_inches = 7.213
+        self.pixels_per_inch = 300
+        self.circle_radius_pixels = 7.213 * self.pixels_per_inch
+        
+        
+        # Do the transform
+        print('Running...')
+        transformed = scipy.ndimage.interpolation.geometric_transform(original_image, self.mapping)
+        print('Done!')
+                
+                
+        # Output the final png        
+        dot_index = file.rfind('.')
+        output_name = file[0:dot_index] + '_output.png'
+        scipy.misc.imsave(output_name,transformed)
+        
+        
+        # Display the png
+        transformed_render = plt.imshow(transformed)
+        plt.gray()
+        plt.show(transformed_render)
     
-    if len(output_coords) == 3:
-        input_coords = input_coords + (output_coords[2],)
-        
-    if len(output_coords) == 4:
-        input_coords = input_coords + (output_coords[3],)
-        
-    return(input_coords)
+    
 
 
+    def mapping(self, output_coords):
 
+        input_coords = (output_coords[0] + (-1 * (math.sqrt(self.circle_radius_pixels ** 2 - (output_coords[1] - (self.original_width_pixels / 2)) ** 2) - self.circle_radius_pixels)), output_coords[1])
+
+        # For color images, there are extra dimensions; we don't need to shift, 
+        # so give 'em back what they gave us.
+        if len(output_coords) == 3:
+            input_coords = input_coords + (output_coords[2],)
+        elif len(output_coords) == 4:
+            input_coords = input_coords + (output_coords[3],)
+    
+        return(input_coords)
+    
+    
+    
 if __name__ == "__main__" :
-    
-    # Load up the image
-    file = argv[1]
-    original_image = scipy.ndimage.imread(file)    
-
-    # Set up measurments
-    original_width_pixels = original_image.shape[1]
-    circle_radius_inches = 7.213
-    pixels_per_inch = 300
-    circle_radius_pixels = 7.213 * pixels_per_inch
+    rectifier = Curved_Lines_Rectifier()
     
     
-    # Do the transform
-    print('Running...')
-    transformed = scipy.ndimage.interpolation.geometric_transform(original_image, mapping)
-    print('Done!')
-            
-    # Output the final png        
-    dot_index = file.rfind('.')
-    output_name = file[0:dot_index] + '_output.png'
-    scipy.misc.imsave(output_name,transformed)
-
-    
-    # Display the png
-    transformed_render = plt.imshow(transformed)
-    plt.gray()
-    plt.show(transformed_render)
