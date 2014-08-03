98740c8dd0b99faa3ca34e02f8d9c528cf888e96
Elliot Lockerman <elliot@lockerman.info>
2014-08-03 13:45:55 -0400

    Refactoring.

--- Curved_Lines_Rectifier.py
+++ Curved_Lines_Rectifier.py
@@ -10,47 +10,28 @@ from math import sqrt
 
 
 
-class Curved_Lines_Rectifier:
+class Curved_Lines_Transformer:
     
-    def __init__(self):
+    def __init__(self, file):
 
         # Load up the image
-        file = argv[1]
-        original_image = scipy.ndimage.imread(file)    
+        self.original_image = scipy.ndimage.imread(file)    
         
         # Set up measurments
-        self.original_width_pixels = original_image.shape[1]
+        self.original_width_pixels = self.original_image.shape[1]
         self.circle_radius_inches = 7.213
         self.pixels_per_inch = 300
         self.circle_radius_pixels = 7.213 * self.pixels_per_inch
         
-        
-        # Do the transform
-        print('Running...')
-        transformed = scipy.ndimage.interpolation.geometric_transform(original_image, self.mapping)
-        print('Done!')
-                
-                
-        # Output the final png        
-        dot_index = file.rfind('.')
-        output_name = file[0:dot_index] + '_output.png'
-        scipy.misc.imsave(output_name,transformed)
-        
-        
-        # Display the png
-        transformed_render = plt.imshow(transformed)
-        plt.gray()
-        plt.show(transformed_render)
     
-    
-
+    def run(self):
+        return scipy.ndimage.interpolation.geometric_transform(self.original_image, self.mapping)
 
     def mapping(self, output_coords):
 
         input_coords = (output_coords[0] + (-1 * (sqrt(self.circle_radius_pixels ** 2 - (output_coords[1] - (self.original_width_pixels / 2)) ** 2) - self.circle_radius_pixels)), output_coords[1])
 
-        # For color images, there are extra dimensions; we don't need to shift, 
-        # so give 'em back what they gave us.
+        # For color images, there are extra dimensions; we don't need to shift, so give 'em back what they gave us.
         if len(output_coords) == 3:
             input_coords = input_coords + (output_coords[2],)
         elif len(output_coords) == 4:
@@ -60,7 +41,27 @@ class Curved_Lines_Rectifier:
     
     
     
-if __name__ == "__main__" :
-    rectifier = Curved_Lines_Rectifier()
+if __name__ == "__main__":
+    
+    file = argv[1]
+    
+    transformer = Curved_Lines_Transformer(file)
+    
+    # Do the transform
+    print('Running...')
+    transformed = transformer.run()
+    print('Done!')
+            
+            
+    # Output the final png        
+    dot_index = file.rfind('.')
+    output_name = file[0:dot_index] + '_output.png'
+    scipy.misc.imsave(output_name,transformed)
+    
+    
+    # Display the png
+    transformed_render = plt.imshow(transformed)
+    plt.gray()
+    plt.show(transformed_render)
     
     
