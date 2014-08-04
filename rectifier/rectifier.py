88369edc13aaa5c27023f0b1261330571c686518
Elliot Lockerman <elliot@lockerman.info>
2014-08-03 22:54:24 -0400

    Added check for output extension.

--- rectifier/rectifier.py
+++ rectifier/rectifier.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from sys import argv
+import sys
 import argparse
 from morph import Transformer
 import utils
@@ -14,15 +14,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="the document to be rectified, as one of the following image formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, or FPX")
     parser.add_argument("--overwrite", help="overwrite any previous output files", action="store_true")
-    parser.add_argument("-f", "--format", help="set output extension to one of the following formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, FPX, or PDF. The default output format is the same as the input format")
+    parser.add_argument("-e", "--extension", help="set output extension to one of the following formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, FPX, or PDF. The default output format is the same as the input format")
     
     # Parse arguments
     args = parser.parse_args()
     original_file_path = args.input
     overwrite = args.overwrite
     
-    if args.format:
-        output_format = args.format # Will be replaced with argument
+    if args.extension:
+        output_format = args.extension # Will be replaced with argument
+        if output_format[0] != '.':
+            sys.stderr.write("Error: output format must be specified as an extension")
+            print("")
+            sys.exit(1)
+            
     else:
         x,args.format = os.path.splitext(origional_file_path)
     
