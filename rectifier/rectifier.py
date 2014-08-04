80147040b6f0969b5296477634b3ed497aadbc79
Elliot Lockerman <elliot@lockerman.info>
2014-08-04 11:49:21 -0400

    Renaming variables for clarity.

--- rectifier/rectifier.py
+++ rectifier/rectifier.py
@@ -14,18 +14,19 @@ if __name__ == "__main__":
     # Get arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="the document to be rectified, as one of the following image formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, or FPX")
-    parser.add_argument("--output", help="Set output path/filename. Default is same as input, with '_output' appended before the extension. If --overwrite is not set and the file already exists '_n' will be appended, where n is the lowest number such that the resulting filename does not yet exist")
-    parser.add_argument("--overwrite", help="overwrite any previous output files", action="store_true")
+    parser.add_argument("--output", help="Set output path/filename. Default is same as input, with '_output' appended before the extension. If --overwrite_argument is not set and the file already exists '_n' will be appended, where n is the lowest number such that the resulting filename does not yet exist")
+    parser.add_argument("--overwrite_argument", help="overwrite_argument any previous output files", action="store_true")
     parser.add_argument("-e", "--extension", help="set output extension to one of the following formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, FPX, or PDF. The default output format is the same as the input format")
     
     # Parse arguments
     args = parser.parse_args()
-    original_file_path = args.input
-    output_file_path = args.output
-    overwrite = args.overwrite
+    original_file_path_argument = args.input
+    output_file_path_argument = args.output
+    overwrite_argument = args.overwrite_argument
+    extension_argument = args.extension
     
-    if args.extension:
-        output_format = args.extension
+    if extension_argument:
+        output_format = extension_argument
         
         if output_format[0] != '.':
             sys.stderr.write("Error: output format must be specified as an extension")
@@ -33,23 +34,24 @@ if __name__ == "__main__":
             sys.exit(1)
             
     else:
-        x,args.format = os.path.splitext(original_file_path) # If the argument wasn't set, the default is the same as the origonal
+        x, output_format = os.path.splitext(original_file_path_argument) # If the argument wasn't set, the default is the same as the origonal
     
     
     # Do the transform
     print('Running...')
-    transformer = Transformer(original_file_path)
+    transformer = Transformer(original_file_path_argument)
     transformed = transformer.run()
     print('Done!')
             
             
     # Output the final png
-    if not output_file_path:
-        output_file_path = utils.generate_output_file_path(original_file_path, overwrite, output_format)
-    elif os.path.exists(output_file_path) and not overwrite:
-        output_file_path = utils.find_earliest_output_path(output_file_path)
-    scipy.misc.imsave(output_file_path,transformed)
-    print("Saved to: " + output_file_path)
+    if not output_file_path_argument:
+        output_file_path_final = utils.generate_output_file_path(original_file_path_argument, overwrite_argument, output_format)
+    elif os.path.exists(output_file_path_argument) and not overwrite_argument:
+        output_file_path_final = utils.find_earliest_output_path(output_file_path_argument)
+    
+    scipy.misc.imsave(output_file_path, transformed)
+    print("Saved to: " + output_file_path_final)
     
     
     # Display the png
