56ed0b28827fd317357d491db8080428e25cc75d
Elliot Lockerman <elliot@lockerman.info>
2014-08-03 23:40:27 -0400

    Clarification of --output option.

--- rectifier/rectifier.py
+++ rectifier/rectifier.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # Get arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="the document to be rectified, as one of the following image formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, or FPX")
-    parser.add_argument("--output", help="Set output path/filename. Default is same as input, with '_output' appended. If --overwrite is not set and the file already exists '_n' will be appended, where n is the lowest number that can be added such that the resulting filename does not exist")
+    parser.add_argument("--output", help="Set output path/filename. Default is same as input, with '_output' appended before the extension. If --overwrite is not set and the file already exists '_n' will be appended, where n is the lowest number such that the resulting filename does not yet exist")
     parser.add_argument("--overwrite", help="overwrite any previous output files", action="store_true")
     parser.add_argument("-e", "--extension", help="set output extension to one of the following formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, FPX, or PDF. The default output format is the same as the input format")
     
