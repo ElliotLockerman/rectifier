934424ab9a369ced5fda8e32331fb7696b4b488d
Elliot Lockerman <elliot@lockerman.info>
2014-08-03 23:38:22 -0400

    Added option to specify output path/filename

--- rectifier/rectifier.py
+++ rectifier/rectifier.py
@@ -14,12 +14,14 @@ if __name__ == "__main__":
     # Get arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="the document to be rectified, as one of the following image formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, or FPX")
+    parser.add_argument("--output", help="Set output path/filename. Default is same as input, with '_output' appended. If --overwrite is not set and the file already exists '_n' will be appended, where n is the lowest number that can be added such that the resulting filename does not exist")
     parser.add_argument("--overwrite", help="overwrite any previous output files", action="store_true")
     parser.add_argument("-e", "--extension", help="set output extension to one of the following formats: BMP, EPS, GIF, IM, JPEG, JPEG 2000, MSP, PCX, PNG, PPM, SPIDER, TIFF, WebP, XBM, XV Thumbnails, DCX, FPX, or PDF. The default output format is the same as the input format")
     
     # Parse arguments
     args = parser.parse_args()
     original_file_path = args.input
+    output_file_path = args.output
     overwrite = args.overwrite
     
     if args.extension:
@@ -42,9 +44,12 @@ if __name__ == "__main__":
             
             
     # Output the final png
-    output_file_path = utils.generate_output_file_path(original_file_path, overwrite, output_format)
+    if not output_file_path:
+        output_file_path = utils.generate_output_file_path(original_file_path, overwrite, output_format)
+    elif os.path.exists(output_file_path) and not overwrite:
+        output_file_path = utils.find_earliest_output_path(output_file_path)
     scipy.misc.imsave(output_file_path,transformed)
-    print("Saved as: " + output_file_path)
+    print("Saved to: " + output_file_path)
     
     
     # Display the png
