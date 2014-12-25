fda791bcd21d01190218f2dba16a0addbc72608a
Elliot Lockerman <elliot@lockerman.info>
2014-12-25 18:00:58 -0500

    Added setuptools script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'rectifier', 
+    version = '1.1',
+    packages = find_packages(),
+    
+    author = 'Elliot Lockerman',
+    author_email = 'elliot@lockerman.info',
+    url = 'http://elliot.lockerman.info/?page=projects/ergonomic_paper/',
+    )
